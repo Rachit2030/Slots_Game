7f23923b3f70c14eb7da4f8df0033a19fa185f24
Rachit2030 <goyalrachit2030@gmail.com>
2021-07-11 07:25:23 +0530

    Initial Commit

---
+++ SlotsGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SlotsGame
+//
+//  Created by Rachit on 11/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SlotsGame/SlotsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotsGameApp.swift
+//  SlotsGame
+//
+//  Created by Rachit on 11/07/21.
+//
+
+import SwiftUI
+
+@main
+struct SlotsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
