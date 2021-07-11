ba3b13a4c609864dd18e0ef0bb68915f8de99876
Rachit2030 <goyalrachit2030@gmail.com>
2021-07-11 07:49:05 +0530

    - Add image to xcassets - Update ContentView

--- SlotsGame/ContentView.swift
+++ SlotsGame/ContentView.swift
@@ -8,9 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var credit = 100
+    @State private var img1 = 1
+    @State private var img2 = 1
+    @State private var img3 = 1
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("SwiftUI Slots!")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+            Spacer()
+            
+            Text("Credits: " + String(credit))
+                .font(.title2)
+            Spacer()
+            
+            HStack{
+                Image("fruit\(img1)" )
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Image("fruit\(img2)")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Image("fruit\(img3)")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+            Spacer()
+            
+            Button(action: {
+                img1 = Int.random(in: 1...3)
+                img2 = Int.random(in: 1...3)
+                img3 = Int.random(in: 1...3)
+                if img1 == img2 && img2 == img3 && img1 == img3{
+                    credit += 50
+                }
+                else{
+                    credit -= 10
+                }
+            }, label: {
+                Text("Spin")
+            })
+            Spacer()
+        }
     }
 }
 
